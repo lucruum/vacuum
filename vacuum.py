a8bea39ba8a8c5e1bdb7d341f2b95d54a6f6588a
Avo <naoh432@mail.ru>
2020-05-28 19:05:26 +0300

    add "new" command

--- vacuum.py
+++ vacuum.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from collections import namedtuple
+import json
+
 # traits
 
 class Show:
@@ -7,12 +11,12 @@ class Show:
 # core
 
 class Package(Show):
-    def __init__(self, *, path, manifest):
+    def __init__(self, path, manifest):
         self.path = path
         self.manifest = manifest
         
 class Manifest(Show):
-    def __init__(self, *, name, version, targets):
+    def __init__(self, name, version, targets):
         self.name = name
         self.version = version
         self.targets = targets
@@ -28,7 +32,7 @@ class Target(Show):
         raise NotImplementedError()
         
 class CompilerTarget(Target):
-    def __init__(self, *, name, build_directory, source_files, include_directories, standart):
+    def __init__(self, name, build_directory, source_files, include_directories, standart):
         self.name = name
         self.build_directory = build_directory
         self.source_files = source_files
@@ -42,5 +46,38 @@ class ExecutableTarget(CompilerTarget):
     pass
     
 # commands
+
+NewOptions = namedtuple('NewOptions', 'name path kind')
+
+def new(options):
+    (options.path / options.name).mkdir()
+    (options.path / options.name / 'src').mkdir()
+    (options.path / options.name / 'include').mkdir()
+    (options.path / options.name / 'include' / options.name).mkdir()
+    
+    with (options.path / options.name / 'vacuum.json').open('w') as file:
+        json.dump({
+            'name': options.name,
+            'version': '0.1',
+            'targets': [
+                {
+                    'name': options.name,
+                    'kind': options.kind,
+                    'source_files': ['src/main.cpp'] if options.kind == 'executable' else [],
+                    'include_directories': ['include'],
+                    'standart': '17',
+                    'build_directory': f'build/{options.name}'
+                }
+            ]
+        }, file, indent=4)
+        
+    if options.kind == 'executable':
+        with (options.path / options.name / 'src' / 'main.cpp').open('w') as file:
+            file.write('#include <iostream>\n')
+            file.write('\n')
+            file.write('int main() {\n')
+            file.write('\tstd::cout << "Hello World!\\n";\n')
+            file.write('\treturn 0;\n')
+            file.write('}')
 
 # main
